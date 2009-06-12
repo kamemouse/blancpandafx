a094998149b39e8ecdc917d5f91dba11e2716123
kamemouse <github@kamemouse.ddo.jp>
2009-06-13 00:30:47 +0900

    modified:   blancpanda/fx/chart/FXChart.java 	new file:   blancpanda/fx/timeperiod/Minute30.java 	new file:   blancpanda/fx/timeperiod/Minute5.java

--- blancpanda/fx/chart/FXChart.java
+++ blancpanda/fx/chart/FXChart.java
@@ -10,7 +10,12 @@ import java.util.List;
 import javax.swing.Timer;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.jfree.chart.JFreeChart;
@@ -26,6 +31,8 @@ import org.jfree.experimental.chart.swt.ChartComposite;
 
 import blancpanda.fx.CandleStick;
 import blancpanda.fx.CandleStickDao;
+import blancpanda.fx.timeperiod.Minute30;
+import blancpanda.fx.timeperiod.Minute5;
 
 public class FXChart {
 
@@ -34,12 +41,51 @@ public class FXChart {
 
 	private OHLCSeries candle;
 	private CandleStick cs;
-	private Date pre_date;
+	private RegularTimePeriod pre_period;
 	private int period;
+	private int currency_pair;
+	private static Combo cmb_period;
+	private static Combo cmb_currency_pair;
 
+	class ChartChanger implements SelectionListener {
+		
+		public void widgetDefaultSelected(SelectionEvent selectionevent) {
+			updateChart();
+		}
+
+		public void widgetSelected(SelectionEvent selectionevent) {
+			updateChart();
+		}
+		
+		private void updateChart() {
+			boolean changed = false;
+			if(period != cmb_period.getSelectionIndex()){
+				// ピリオドが変わった
+				period = cmb_period.getSelectionIndex();
+				changed = true;
+			}
+			if(currency_pair != cmb_currency_pair.getSelectionIndex()){
+				// 通貨ペアが変わった
+				currency_pair = cmb_currency_pair.getSelectionIndex();
+				changed = true;
+			}
+			if(changed){
+				// ピリオドか通貨ペアが変わったら
+				// CandleStickを書き換える
+				cs = new CandleStick(currency_pair, period);
+				// ロウソク足を全部消す
+				candle.clear();
+				// DBから読み込み直す
+				loadCandleStick();
+				// チャートの書き直し
+				chartComposite.forceRedraw();
+				changed = false;
+			}
+		}
+	}
 	class DataGenerator extends Timer implements ActionListener {
 		/**
-		 *
+		 * 
 		 */
 		private static final long serialVersionUID = 200906080L;
 
@@ -52,7 +98,7 @@ public class FXChart {
 			// [SILENT] for SWT UI thread issue, use Display.asyncExec()
 			display.asyncExec(new Runnable() {
 				public void run() {
-					addCandleStick();
+					updateCandleStick();
 
 					// [SILENT] for chart redraw, use
 					// ChartComposite.forceRedraw();
@@ -72,11 +118,32 @@ public class FXChart {
 		candle.setMaximumItemCount(max);
 		cs = new CandleStick(currency_pair, period);
 		this.period = period;
-		pre_date = new Date();
+		this.currency_pair = currency_pair;
+		// pre_periodの初期化
+		pre_period = getRegularTimePeriod(new Date());
+	}
+
+	private RegularTimePeriod getRegularTimePeriod(Date date) {
+		RegularTimePeriod ret = null;
+		switch (period) {
+		case CandleStick.M1:
+			ret = new Minute(date);
+			break;
+		case CandleStick.M5:
+			ret = new Minute5(date);
+			break;
+		case CandleStick.M30:
+			ret = new Minute30(date);
+			break;
+		default:
+			System.out.println("ピリオドが不正です。");
+			break;
+		}
+		return ret;
 	}
 
 	public JFreeChart createChart() {
-		//loadCandleStick();
+		loadCandleStick();
 
 		OHLCSeriesCollection dataset = new OHLCSeriesCollection();
 		dataset.addSeries(candle);
@@ -103,34 +170,44 @@ public class FXChart {
 	}
 
 	private void loadCandleStick() {
+/*
 		CandleStickDao csDao = new CandleStickDao();
 		List<CandleStick> list = csDao.getRecentList(period);
 		int serice = list.size();
 		for (int i = serice - 1; i >= 0; i--) { // 時間の降順で取得してくる
 			cs = list.get(i);
-			candle.add(new Minute(cs.getDate()), cs.getBid_open(), cs
-					.getBid_high(), cs.getBid_low(), cs.getBid_close());
+			// DBに重複データができてしまっている可能性がある
+			RegularTimePeriod prd = getRegularTimePeriod(cs.getDate());
+			int index = candle.indexOf(prd);
+//			System.out.println("index:" + index);
+			if (index >= 0) {
+				candle.remove(prd);
+			}
+			candle.add(prd, cs.getBid_open(), cs.getBid_high(),
+					cs.getBid_low(), cs.getBid_close());
 		}
+*/
 	}
 
-	private void addCandleStick() {
+	@SuppressWarnings("unchecked")
+	private void updateCandleStick() {
 		Date date = cs.getCurrentRate();
-		System.out.println(date);
-		if(date.compareTo(pre_date) >= 0){
-			// なぜか取得日時が逆戻りするので、戻った場合には何もしない。
-			RegularTimePeriod prd = new Minute(date);
+//		System.out.println(date);
+		// なぜか取得日時が逆戻りするので、戻った場合には何もしない。
+		RegularTimePeriod prd = getRegularTimePeriod(date);
+		if (prd.compareTo(pre_period) >= 0) {
 			int index = candle.indexOf(prd);
-			System.out.println("index:" + index);
+//			System.out.println("index:" + index);
 			if (index >= 0) {
 				candle.remove(prd);
 			} else {
 				cs.clearRate();
 			}
-			candle.add(prd, cs.getBid_open(),
-					cs.getBid_high(), cs.getBid_low(), cs.getBid_close());
-			pre_date = date;
-		}else{
-			System.out.println("逆戻り");
+			candle.add(prd, cs.getBid_open(), cs.getBid_high(),
+					cs.getBid_low(), cs.getBid_close());
+			pre_period = prd;
+		} else {
+//			System.out.println("逆戻り");
 		}
 	}
 
@@ -138,16 +215,60 @@ public class FXChart {
 		display = new Display();
 		Shell shell = new Shell(display);
 		shell.setSize(600, 480);
-		shell.setLayout(new FillLayout());
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		layout.marginHeight = 4;
+		layout.marginWidth = 4;
+		shell.setLayout(layout);
 		shell.setText("リアルタイム為替チャート");
-
-		FXChart fxchart = new FXChart(CandleStick.M1, CandleStick.USDJPY, 60);
+		
+		Composite toolbar = new Composite(shell, SWT.NONE);
+		GridLayout lo_toolbar = new GridLayout();
+		lo_toolbar.numColumns = 2;
+		toolbar.setLayout(lo_toolbar);
+		GridData ld_toolbar = new GridData(GridData.FILL_HORIZONTAL);
+		toolbar.setLayoutData(ld_toolbar);
+		
+		FXChart fxchart = new FXChart(CandleStick.M5, CandleStick.USDJPY, 60);
 		JFreeChart chart = fxchart.createChart();
+
+		// 通貨ベア選択コンボの追加
+		cmb_currency_pair = new Combo(toolbar,SWT.DROP_DOWN|SWT.BORDER|SWT.READ_ONLY);
+		cmb_currency_pair.addSelectionListener(fxchart.new ChartChanger());
+		cmb_currency_pair.add("USD/CAD");
+		cmb_currency_pair.add("EUR/JPY");
+		cmb_currency_pair.add("NZD/JPY");
+		cmb_currency_pair.add("GBP/CHF");
+		cmb_currency_pair.add("USD/CHF");
+		cmb_currency_pair.add("ZAR/JPY");
+		cmb_currency_pair.add("NZD/USD");
+		cmb_currency_pair.add("CAD/JPY");
+		cmb_currency_pair.add("EUR/GBP");
+		cmb_currency_pair.add("USD/JPY");	// 米ドル円
+		cmb_currency_pair.add("CHF/JPY");
+		cmb_currency_pair.add("GBP/JPY"); 	// 英ポンド円
+		cmb_currency_pair.add("GBP/USD");
+		cmb_currency_pair.add("AUD/JPY");
+		cmb_currency_pair.add("EUR/USD");
+		cmb_currency_pair.add("AUD/USD");
+		cmb_currency_pair.select(CandleStick.USDJPY);
+//		cmb_currency_pair.setEnabled(false);	// とりあえず、米ドル円のみ
+
+		// ピリオド選択コンボの追加
+		cmb_period = new Combo(toolbar,SWT.DROP_DOWN|SWT.BORDER|SWT.READ_ONLY);
+		cmb_period.addSelectionListener(fxchart.new ChartChanger());
+		cmb_period.add("M1");
+		cmb_period.add("M5");
+		cmb_period.add("M30");
+		cmb_period.select(CandleStick.M5);
 
 		chartComposite = new ChartComposite(shell, SWT.NONE, chart, true);
 		chartComposite.setDisplayToolTips(true);
 		chartComposite.setHorizontalAxisTrace(false);
 		chartComposite.setVerticalAxisTrace(false);
+		GridData ld_chart = new GridData(GridData.FILL_BOTH);
+		chartComposite.setLayoutData(ld_chart);
+		
 		shell.open();
 
 		Timer timer = fxchart.new DataGenerator(1000);
@@ -157,5 +278,8 @@ public class FXChart {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
+		
+		timer.stop();
+		display.dispose();
 	}
 }
